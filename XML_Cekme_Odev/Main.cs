4d10a8348367625c70e74ee3c0df95d1285e56eb
ufukopak <ufuk.opak34@gmail.com>
2021-04-25 13:26:38 +0300

    Bildirim Yapmayı Unutmuşum Ekledim

--- XML_Cekme_Odev/Main.cs
+++ XML_Cekme_Odev/Main.cs
@@ -17,6 +17,8 @@ namespace XML_Cekme_Odev
         {
             InitializeComponent();
         }
+        string eskiVeri;
+
         private void lbBasliklar_DoubleClick(object sender, EventArgs e)
         {
             tbBaslik.Text = lbBasliklar.Items[lbBasliklar.SelectedIndex].ToString();
@@ -27,18 +29,26 @@ namespace XML_Cekme_Odev
             HaberDetay haberDetay = new HaberDetay();
             haberDetay.baslik = tbBaslik.Text;
             haberDetay.Show();
+
         }
 
         private void timerXML_Tick(object sender, EventArgs e)
         {
             lbBasliklar.Items.Clear();
             XmlTextReader xmlOku = new XmlTextReader("http://www.star.com.tr/rss/sondakika.xml");
+            string temp = eskiVeri;
+            eskiVeri = "";
             while (xmlOku.Read())
             {
                 if (xmlOku.Name == "title")
                 {
                     lbBasliklar.Items.Add(xmlOku.ReadString());
+                    eskiVeri += xmlOku.ReadString();
                 }
+            }
+            if (eskiVeri != temp)
+            {
+                MessageBox.Show("Yeni Haber Geldi !","Yeni Bildirimi",MessageBoxButtons.OK,MessageBoxIcon.Warning);
             }
         }
 
