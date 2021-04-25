b9b050ad08fd3a966a2377920f7e67f6f0090694
ufukopak <ufuk.opak34@gmail.com>
2021-04-25 12:20:39 +0300

    Haber Detaylarını göstermek için tasarım yaptım

---
+++ XML_Cekme_Odev/HaberDetay.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace XML_Cekme_Odev
+{
+    partial class HaberDetay
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.lblBaslik = new System.Windows.Forms.Label();
+            this.lblDetay = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(538, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(147, 109);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(509, 246);
+            this.textBox1.TabIndex = 1;
+            // 
+            // lblBaslik
+            // 
+            this.lblBaslik.AutoSize = true;
+            this.lblBaslik.Location = new System.Drawing.Point(220, 12);
+            this.lblBaslik.Name = "lblBaslik";
+            this.lblBaslik.Size = new System.Drawing.Size(35, 13);
+            this.lblBaslik.TabIndex = 2;
+            this.lblBaslik.Text = "label1";
+            // 
+            // lblDetay
+            // 
+            this.lblDetay.AutoSize = true;
+            this.lblDetay.Location = new System.Drawing.Point(12, 84);
+            this.lblDetay.Name = "lblDetay";
+            this.lblDetay.Size = new System.Drawing.Size(35, 13);
+            this.lblDetay.TabIndex = 3;
+            this.lblDetay.Text = "Detay";
+            // 
+            // HaberDetay
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(697, 358);
+            this.Controls.Add(this.lblDetay);
+            this.Controls.Add(this.lblBaslik);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "HaberDetay";
+            this.Text = "HaberDetay";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label lblBaslik;
+        private System.Windows.Forms.Label lblDetay;
+    }
+}
---
+++ XML_Cekme_Odev/HaberDetay.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XML_Cekme_Odev
+{
+    public partial class HaberDetay : Form
+    {
+        public HaberDetay()
+        {
+            InitializeComponent();
+        }
+
+    }
+}
